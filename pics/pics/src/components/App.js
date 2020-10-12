516d8f9881662b7012cf0621552af4e6f77816c0
nourall22 <nidorigin@gmail.com>
2020-10-12 20:18:34 +0100

    pics

--- pics/pics/src/components/App.js
+++ pics/pics/src/components/App.js
@@ -1,11 +1,27 @@
 import React from "react";
+import axios from 'axios';
 import SearchBar from "./SearchBar";
 
-const App = () => {
-  return (
-    <div className="ui container" style={{ marginTop: "10px" }}>
-      <SearchBar />
-    </div>
-  );
-};
-export default App;
+
+class App extends React.Component {
+  onSearchSubmit(term){
+    axios.get('https://api.unsplash.com/search/photos',{
+      params:{query: term},
+      headers:{
+        Authorization: 'Client-ID R5TJfC3kcdNB6Kn__BqQxGfmusimKp316XHeWXRKirg' 
+      }
+    });
+  }
+  render() {
+    return (
+      <div className="ui container" style={{marginTop:'10px'}} >
+        <SearchBar onSubmitSearch= {this.onSearchSubmit} />
+      </div>
+    );
+  }
+}
+
+export default App
+
+
+
